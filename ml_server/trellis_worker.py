2a867e99b0109fb01a2a14e7a2e7a225a88f7d9c
Kiril Kaniushyk <kikanbig@gmail.com>
2025-09-18 14:04:56 +0300

    fix: resolve werkzeug/dash version conflict

--- ml_server/trellis_worker.py
+++ ml_server/trellis_worker.py
@@ -1,6 +1,6 @@
 """
 TRELLIS Worker for 3D Generation
-Version: 2024-09-18-12:40 (install utils3d separately - fix missing dependency)
+Version: 2024-09-18-12:50 (fix werkzeug/dash version conflict)
 """
 import os
 import sys
